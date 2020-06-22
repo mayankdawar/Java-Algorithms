361c06ad7a125baca57b53204d3091e8fb9a7550
Mayank Dawar <mayankdawar29@gmail.com>
2020-06-23 01:30:25 +0530

    Update and rename Stack and Queues/Stack/Link list Implementation/Main.java to Stack and Queues/Stack/Linked list ImplementationMain.java

--- Stack and Queues/Stack/Linked list ImplementationMain.java
+++ Stack and Queues/Stack/Linked list ImplementationMain.java
@@ -1,28 +1,27 @@
-package com.company;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         LinkedStack obj = new LinkedStack();
         Scanner scanner =new Scanner(System.in);
         boolean cond = true;
 
-        System.out.println("Press:" +
-                "1: For pushing in Stack\n" +
-                "2: For pop out from stack\n" +
+        System.out.println("Press:\n" +
+                "1: For adding item in Queue\n" +
+                "2: For removing item from Queue\n" +
                 "3: to exit\n" +
-                "4: to print this info");
+                "4: to print this info\n" +
+                "5: to print the queue");
         while (cond){
             int cho = scanner.nextInt();
             switch (cho){
                 case 1: {
                     String str = scanner.next();
-                        obj.push(str);
-                        System.out.println("String added Successfully");
-                        break;
+                    obj.push(str);
+                    System.out.println("String added Successfully");
+                    break;
                 }
                 case 2:{
                     if (obj.isEmpty()){
@@ -37,16 +36,18 @@ public class Main {
                     break;
                 }
                 case 4:{
-                    System.out.println("Press:" +
-                            "1: For pushing in Stack\n" +
-                            "2: For pop out from stack\n" +
-                            "3: to exit\n" +
-                            "4: to print this info");
-                    break;
-                }
-                case 5:
                     obj.print();
                     break;
+                }
+                case 5:{
+                    System.out.println("Press:\n" +
+                            "1: To push item into Stack\n" +
+                            "2: To pop item from Stack\n" +
+                            "3: to exit\n" +
+                            "4: to print the queue"+
+                            "5: to print this info\n");
+                    break;
+                }
                 default:
                     cond = true;
                     break;
